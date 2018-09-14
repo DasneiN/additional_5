d7d98e165ba9d6e45f3f77a0e475e42c296c32a8
DasneiN <sank.mail@gmail.com>
2018-09-14 22:35:26 +0300

    Clear Solution 20/20

--- src/index.js
+++ src/index.js
@@ -1,30 +1,5 @@
 module.exports = function check(str, bracketsConfig) {
 
-  // while (str.length > 0) {
-
-  //   for (let j = 0; j < bracketsConfig.length; j++) {
-  //     let sliced = false;
-      
-  //     if (str[0] == bracketsConfig[j][0]) {
-
-  //       if (str[1] == bracketsConfig[j][1]) {
-  //         str = str.slice(3);
-  //         sliced = true;
-  //       } else if (str[str.length - 1] == bracketsConfig[j][1]) {
-  //         str = str.slice(1,-1);
-  //         sliced = true;
-  //       }
-        
-  //     }
-
-  //     if (j == bracketsConfig.length - 1 && !sliced) {
-  //       return false;
-  //     }
-      
-  //   }
-    
-  // }
-
   while (str.length > 0) {
     let replaced = false;
     
